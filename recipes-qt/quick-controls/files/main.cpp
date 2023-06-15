51b91b072c6c701c3db2ab36373b1fd4b6c20d5e
OOHehir <40183643+OOHehir@users.noreply.github.com>
2023-06-15 13:19:26 +0100

    Fix recipe

--- recipes-qt/quick-controls/files/main.cpp
+++ recipes-qt/quick-controls/files/main.cpp
@@ -10,10 +10,14 @@ int main(int argc, char *argv[])
     QQuickStyle::setStyle("Material");
 
     QQmlApplicationEngine engine;
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("qmlDemo", "Main");
+    engine.load(QUrl::fromLocalFile("Main.qml"));
+//    const QUrl url(u"qrc:/qt-qml-demo/Main.qml"_qs);
+//    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+//        &app, [url](QObject *obj, const QUrl &objUrl) {
+//            if (!obj && url == objUrl)
+//                QCoreApplication::exit(-1);
+//        }, Qt::QueuedConnection);
+//    engine.load(url);
 
     return app.exec();
 }
